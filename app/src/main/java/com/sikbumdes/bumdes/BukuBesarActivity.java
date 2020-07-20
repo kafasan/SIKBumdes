86db5a3dc812718a585968982879c95164017f7a
unknown <afyadkf@gmail.com>
2020-07-20 08:43:27 +0700

    first commit

---
+++ app/src/main/java/com/sikbumdes/bumdes/BukuBesarActivity.java
@@ -0,0 +1,14 @@
+package com.sikbumdes.bumdes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BukuBesarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buku_besar);
+    }
+}
---
+++ app/src/main/java/com/sikbumdes/bumdes/JurnalActivity.java
@@ -0,0 +1,14 @@
+package com.sikbumdes.bumdes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JurnalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jurnal);
+    }
+}
